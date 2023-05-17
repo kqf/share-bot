e99d5f228c24daf91c4f3a3342092940acdae8ab
bobo-bob <q.dev.null@gmail.com>
2023-05-17 08:14:49 +0100

    Cleanup the main function

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import logging
 
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Application, CommandHandler, MessageHandler, Filters
 
 from bot.commands import start, helpme, thanks, forward
 from bot.settings import config
@@ -9,37 +9,33 @@ from bot.settings import config
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
 
 def main():
-    updater = Updater(config.token)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", helpme))
-    dp.add_handler(CommandHandler("copyright", forward))
-    dp.add_handler(CommandHandler("forward", forward))
-    dp.add_handler(MessageHandler(~Filters.command, thanks))
+    app = Application.builder().token(config.token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", helpme))
+    app.add_handler(CommandHandler("copyright", forward))
+    app.add_handler(CommandHandler("forward", forward))
+    app.add_handler(MessageHandler(~Filters.command, thanks))
 
     # No webhook -- run in the debug mode
     if config.webhook is None:
-        updater.start_polling()
-        updater.idle()
+        app.run_polling()
+        app.idle()
         return
 
-    updater.start_webhook(
+    app.run_polling(
         listen="0.0.0.0",
         port=config.port,
-        url_path=config.token
+        url_path=config.token,
+        webhook_url=config.webhook,
     )
-    updater.bot.set_webhook(config.webhook)
-    updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
