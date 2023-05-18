b81f90916caa11281f5af4e26aff71fe6f467e7e
bobo-bob <q.dev.null@gmail.com>
2023-05-18 07:15:39 +0100

    Fix the broken tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -3,7 +3,16 @@ import os
 from unittest.mock import patch
 
 
-
-@patch.dict(os.environ, {"TOKEN": "FakeToken", "ADMIN_IDS":"1,2", "THANKS_STICKERS": 1,})
+@patch.dict(
+    os.environ,
+    {
+        "TOKEN": "FakeToken",
+        "ADMIN_IDS": "1,2",
+        "THANKS_STICKERS": "1",
+        "MESSAGE_THANKS": "Thanks",
+        "MESSAGE_START": "Hey",
+        "MESSAGE_HELP": "Help",
+    },
+)
 def test_imports():
     from bot.main import main  # noqa
