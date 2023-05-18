a9f8ad325970b80b6774969d604935193973e78f
bobo-bob <q.dev.null@gmail.com>
2023-05-18 07:13:13 +0100

    Fix the dummy tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,5 +1,9 @@
-from bot.main import main  # noqa
+import os
+
+from unittest.mock import patch
 
 
+
+@patch.dict(os.environ, {"TOKEN": "FakeToken", "ADMIN_IDS":"1,2", "THANKS_STICKERS": 1,})
 def test_imports():
-    pass
+    from bot.main import main  # noqa
