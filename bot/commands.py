9872f36f77fc002b164731c1c59ea1f90399d4e3
bobo-bob <q.dev.null@gmail.com>
2023-05-19 14:48:57 +0100

    Add the async keyword

--- bot/commands.py
+++ bot/commands.py
@@ -3,26 +3,26 @@ import random
 from bot.settings import config
 
 
-def _forward_to_admins(update, context):
+async def _forward_to_admins(update, context):
     for admin_id in config.admin_ids:
         update.message.forward(admin_id)
 
 
-def start(update, context):
+async def start(update, context):
     update.message.reply_text(config.message_start)
 
 
-def helpme(update, context):
+async def helpme(update, context):
     update.message.reply_text(config.message_help)
 
 
-def forward(update, context):
+async def forward(update, context):
     _forward_to_admins(update, context)
     stem = update.message.text[1:]
     update.message.reply_text(f"Your next message will be {stem}ed :)")
 
 
-def thanks(update, context):
+async def thanks(update, context):
     _forward_to_admins(update, context)
     update.message.reply_text(config.message_thanks)
     update.message.reply_sticker(random.choice(config.thanks_stickers))
